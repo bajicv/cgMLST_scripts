9df26fb00d98889cee3272ea535691b0018ec1ea
Vladimir Bajić <44064386+bajicv@users.noreply.github.com>
2024-04-03 18:53:33 +0200

    Update compare_two_cgMLST_profiles_plots.R

--- compare_two_cgMLST_profiles_plots.R
+++ compare_two_cgMLST_profiles_plots.R
@@ -1,14 +1,13 @@
 ################################################################################
-# Compare two cgMLST profiles for same set of samples and loci
+# Plot results of compare_two_cgMLST_profiles.R
 #
 # Author: Vladimir Bajić
 # Date: 2024-04-03
 #
 # Description:
 # This script
-#   - Compares two cgMLST profiles for the same set of samples and loci
-#   - Outputs comparison summary per locus (suffix: _LCOMP.tsv)
-#   - Outputs comparison summary per sample (suffix: _SCOMP.tsv)
+#   - Outputs a plot summarizing comparison per locus (suffix: _LCOMP.png)
+#   - Outputs a plot summarizing comparison per sample (suffix: _SCOMP.png)
 #
 # Categories used in summary:
 #   S   - same allele call in both profiles
@@ -22,160 +21,114 @@
 # Usage:
 #
 # To see help message
-#   Rscript --vanilla compare_two_cgMLST_profiles.R --help
+#   Rscript --vanilla compare_two_cgMLST_profiles_plot.R --help
 #
-# To compare two profiles
-#   Rscript --vanilla compare_two_cgMLST_profiles.R --f first.profile --s second.profile -o output_path
+# To run the script it is enough to provide path to either LCOMP or SCOMP (assuming that LCOMP and SCOMP are in the same directory)
+#   Rscript --vanilla compare_two_cgMLST_profiles_plot.R -l LCOMP.tsv
+#   Rscript --vanilla compare_two_cgMLST_profiles_plot.R -s SCOMP.tsv
+#
+# To run the script with specific output path, threshold and input paths for LCOMP and SCOMP
+#   Rscript --vanilla compare_two_cgMLST_profiles_plot.R -l LCOMP.tsv -s SCOMP.tsv - o out_path -t 95
 #
 ################################################################################
 
 # Libraries  -------------------------------------------------------------------
 suppressMessages(library(tidyverse))
+suppressMessages(library(ggpubr))
 library(optparse)
-
-# To suppress summarize information
-options(dplyr.summarise.inform = FALSE)
 
 # Making option list -----------------------------------------------------------
 option_list <- list(
-    make_option(c("-f", "--first_profile"), type = "character", help = "Path to the first cgMLST profile", metavar = "character"),
-    make_option(c("-s", "--second_profile"), type = "character", help = "Path to the second cgMLST profile", metavar = "character"),
-    make_option(c("-o", "--output"), type = "character", help = "Output name prefix", metavar = "character")
+    make_option(c("-l", "--lcomp"), type = "character", help = "Path to LCOMP file [tsv].", metavar = "character"),
+    make_option(c("-s", "--scomp"), type = "character", help = "Path to SCOMP file [tsv].", metavar = "character"),
+    make_option(c("-t", "--threshold"), type = "numeric", help = "Threshould value [1-100]. Plot only loci with the same allele calls in less than provided threshould value. Default value = 90", metavar = "numeric", default = 90),
+    make_option(c("-o", "--output"), type = "character", help = "Output name prefix.", metavar = "character")
 )
 
 # Parsing options
 opt_parser <- OptionParser(option_list = option_list)
 opt <- parse_args(opt_parser)
 
+
 # Check the provided option and execute the corresponding code -----------------
-if (is.null(opt$f)) {
+if (is.null(opt$l) & is.null(opt$s)) {
     print_help(opt_parser)
-    stop("Provide path to the first profile file.")
+    stop("Provide path to LCOMP or SCOMP file.")
 }
 
+# Check if path to SCOMP is specified and if not use LCOMP to define it
 if (is.null(opt$s)) {
-    print_help(opt_parser)
-    stop("Provide path to the second profile file.")
+    cat("SCOMP not specified.\nSCOMP will be searched in the same folder where is LCOMP.\n")
+    opt$s <- str_replace(opt$l, "_LCOMP.tsv$", "_SCOMP.tsv")
 }
 
+# Check if path to LCOMP is specified and if not use SCOMP to define it
+if (is.null(opt$l)) {
+    cat("LCOMP not specified.\nSCOMP will be searched in the same folder where is SCOMP.\n")
+    opt$l <- str_replace(opt$s, "_SCOMP.tsv$", "_LCOMP.tsv")
+}
+
+# Check if output is specified and if not use input to define it
 if (is.null(opt$o)) {
-    print_help(opt_parser)
-    stop("Output path must be provided.")
+    opt$o <- dirname(opt$l)
+    cat("Output not specified.\nOutput will be saved in: ", opt$o, "\n", sep = "")
 }
 
 # ------------------------------------------------------------------------------
 
-
 # Load data --------------------------------------------------------------------
-cat("Loading profile 1 ...\n")
-profile_1 <- read_tsv(opt$f, show_col_types = FALSE) %>% rename_with(.cols = 1, ~"Sample")
-
-cat("Loading profile 2 ...\n")
-profile_2 <- read_tsv(opt$s, show_col_types = FALSE) %>% rename_with(.cols = 1, ~"Sample")
-
-# Test if samples are the same -------------------------------------------------
-if (sum(!c(profile_1$Sample %in% profile_2$Sample)) + sum(!c(profile_2$Sample %in% profile_1$Sample)) == 0) {
-    cat("Same sample IDs present in both profiles.\n")
-    if (nrow(profile_1) != nrow(profile_2)) {
-        cat("WARNING: Different number of samples in profiles! \n")
-    }
-    if (nrow(profile_1) == nrow(profile_2)) {
-        cat("Same number of samples in both profiles.\n")
-    }
-} else {
-    cat("WARNING: Samples in both profiles are NOT identical!")
-}
-
-# Print number of samples in each profile --------------------------------------
-cat("Number of samples in profile 1: ", nrow(profile_1), "\n")
-cat("Number of samples in profile 2: ", nrow(profile_2), "\n")
-
-# Test if samples are duplicated and list those that are -----------------------
-if (sum(duplicated(profile_1$Sample)) > 0) {
-    cat("Duplicated samples detected in profile 1: ", profile_1$Sample[duplicated(profile_1$Sample)], "\n")
-}
-if (sum(duplicated(profile_2$Sample)) > 0) {
-    cat("Duplicated samples detected in profile 2: ", profile_2$Sample[duplicated(profile_2$Sample)], "\n")
-}
-
-# Test if loci are the same ----------------------------------------------------
-
-p1_notin_p2 <- sum(!c(names(profile_1) %in% names(profile_2)))
-p2_notin_p1 <- sum(!c(names(profile_2) %in% names(profile_1)))
-
-if (p1_notin_p2 + p2_notin_p1 == 0) {
-    cat("Loci in both profiles are identical.\n")
-} else {
-    cat("WARNING: Loci in both profiles are NOT identical!\n")
-    if (p1_notin_p2 > 0) {
-        cat("Number of loci from profile 1 not present in profile 2:", p1_notin_p2, "\n")
-    }
-    if (p2_notin_p1 > 0) {
-        cat("Number of loci from profile 2 not present in profile 1:", p2_notin_p1, "\n")
-    }
-}
-
-# Print number of loci in each profile -----------------------------------------
-cat("Number of loci in profile 1: ", ncol(profile_1), "\n")
-cat("Number of loci in profile 2: ", ncol(profile_2), "\n")
-
-# Test if loci are duplicated and list those that are --------------------------
-if (sum(duplicated(names(profile_1))) > 0) {
-    cat("Duplicated loci detected in profile 1: ", names(profile_1)[duplicated(names(profile_1))], "\n")
-}
-if (sum(duplicated(names(profile_2))) > 0) {
-    cat("Duplicated loci detected in profile 2: ", names(profile_2)[duplicated(names(profile_2))], "\n")
-}
-
-# Convert to long formats ------------------------------------------------------
-lp1 <- pivot_longer(profile_1, cols = -1, names_to = "Locus", values_to = "Allele_1")
-lp2 <- pivot_longer(profile_2, cols = -1, names_to = "Locus", values_to = "Allele_2")
+cat("Reading in the data.\n")
+LCOMP <- read_tsv(opt$l, show_col_types = FALSE)
+SCOMP <- read_tsv(opt$s, show_col_types = FALSE)
 
 
-# Join two profiles ------------------------------------------------------------
-comp <- full_join(lp1, lp2, by = c("Sample", "Locus"))
+# Plot SAMPLE ------------------------------------------------------------------
 
-# Make classifications ---------------------------------------------------------
-classified_comp <- comp %>%
-    mutate(
-        Classification = case_when(
-            Allele_1 == Allele_2 & Allele_1 != 0 & Allele_2 != 0 ~ "S",
-            Allele_1 != Allele_2 & Allele_1 != 0 & Allele_2 != 0 ~ "D",
-            Allele_1 == Allele_2 & Allele_1 == 0 & Allele_2 == 0 ~ "M",
-            Allele_1 != Allele_2 & Allele_1 == 0 & Allele_2 != 0 ~ "M1",
-            Allele_1 != Allele_2 & Allele_1 != 0 & Allele_2 == 0 ~ "M2",
-            is.na(Allele_1) & !is.na(Allele_2) ~ "X1",
-            !is.na(Allele_1) & is.na(Allele_2) ~ "X2",
-            TRUE ~ "Unknown"
-        )
+my_colors <-
+    c(
+        "M" = "#1f78b4",
+        "M1" = "#ffff99",
+        "M2" = "#fdbf6f",
+        "S" = "#33a02c",
+        "D" = "#e31a1c",
+        "X1" = "#969696",
+        "X2" = "#252525"
     )
 
-# List of expected Classification column names ---------------------------------
-classifications <- c("S", "D", "M", "M1", "M2", "X1", "X2") %>% map_dfr(~ tibble(!!.x := logical()))
+cat("Plotting SCOMP.\n")
+p_s <- SCOMP %>%
+    arrange(desc(S), D, M, M1, M2, X1, X2) %>%
+    pivot_longer(cols = -1, names_to = "Type", values_to = "Count") %>%
+    mutate(Sample = forcats::fct_inorder(Sample)) %>%
+    ggplot(aes(x = Count, y = Sample, fill = Type)) +
+    geom_bar(stat = "identity") +
+    labs(
+        title = "Comparisons per SAMPLE",
+        x = "Loci",
+        y = "Sample"
+    ) +
+    scale_fill_manual(values = my_colors) +
+    theme_bw()
 
-# Comparison per Sample --------------------------------------------------------
-s_comp <-
-    classified_comp %>%
-    group_by(Sample, Classification) %>%
-    summarise(n = n()) %>%
-    pivot_wider(names_from = "Classification", values_from = "n", values_fill = 0) %>%
-    bind_rows(., classifications) %>%
-    replace(is.na(.), 0) %>%
-    select("Sample", "S", "D", "M", "M1", "M2", "X1", "X2") %>%
-    arrange(S, desc(D), desc(M), desc(M1), desc(M2), desc(X1), desc(X2))
+# Plot LOCUS -------------------------------------------------------------------
+cat("Plotting LCOMP.\n")
+p_l <- LCOMP %>%
+    arrange(desc(S), D, M, M1, M2, X1, X2) %>%
+    filter(S < nrow(SCOMP) * (opt$t) / 100) %>%
+    pivot_longer(cols = -1, names_to = "Type", values_to = "Count") %>%
+    mutate(Locus = forcats::fct_inorder(Locus)) %>%
+    ggplot(aes(x = Count, y = Locus, fill = Type)) +
+    geom_bar(stat = "identity") +
+    labs(
+        title = paste0("Comparisons per LOCUS"),
+        subtitle = paste0("Shown are only loci with the same allele calls in less than ", opt$t, "% of samples"),
+        x = "Samples",
+        y = "Locus",
+    ) +
+    scale_fill_manual(values = my_colors) +
+    theme_bw()
 
-# Comparison per Locus ---------------------------------------------------------
-l_comp <-
-    classified_comp %>%
-    group_by(Locus, Classification) %>%
-    summarise(n = n()) %>%
-    pivot_wider(names_from = "Classification", values_from = "n", values_fill = 0) %>%
-    bind_rows(., classifications) %>%
-    replace(is.na(.), 0) %>%
-    select("Locus", "S", "D", "M", "M1", "M2", "X1", "X2") %>%
-    arrange(S, desc(D), desc(M), desc(M1), desc(M2), desc(X1), desc(X2))
-
-# Save output ------------------------------------------------------------------
-cat("Saving the tables as: ", paste0(opt$o, "_profile_{LCOMP,SCOMP}.tsv", "\n"))
-write_tsv(l_comp, paste0(opt$o, "_profile_LCOMP.tsv"))
-write_tsv(s_comp, paste0(opt$o, "_profile_SCOMP.tsv"))
+# Save plots -------------------------------------------------------------------
+cat("Saving plots.\n")
+ggsave(paste0(opt$o, "/", tools::file_path_sans_ext(basename(opt$s)), ".png"), p_s, width = 10, height = 20)
+ggsave(paste0(opt$o, "/", tools::file_path_sans_ext(basename(opt$l)), ".png"), p_l, width = 10, height = 20)
