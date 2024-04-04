f18d8c6f076774c08de9fc02e851d137f4149396
Vladimir Bajić <44064386+bajicv@users.noreply.github.com>
2024-04-04 15:10:53 +0200

    Update compare_two_cgMLST_profiles_plots.R

--- compare_two_cgMLST_profiles_plots.R
+++ compare_two_cgMLST_profiles_plots.R
@@ -2,10 +2,11 @@
 # Plot results of compare_two_cgMLST_profiles.R
 #
 # Author: Vladimir Bajić
-# Date: 2024-04-03
+# Date: 2024-04-04
 #
 # Description:
 # This script
+#   - Outputs a plot summarizing general comparison between two profiles (suffix: _GCOMP.png)
 #   - Outputs a plot summarizing comparison per locus (suffix: _LCOMP.png)
 #   - Outputs a plot summarizing comparison per sample (suffix: _SCOMP.png)
 #
@@ -23,12 +24,13 @@
 # To see help message
 #   Rscript --vanilla compare_two_cgMLST_profiles_plot.R --help
 #
-# To run the script it is enough to provide path to either LCOMP or SCOMP (assuming that LCOMP and SCOMP are in the same directory)
+# To run the script it is enough to provide path to either GCOMP or LCOMP or SCOMP (assuming that those 3 files are in the same directory)
+#   Rscript --vanilla compare_two_cgMLST_profiles_plot.R -g GCOMP.tsv
 #   Rscript --vanilla compare_two_cgMLST_profiles_plot.R -l LCOMP.tsv
 #   Rscript --vanilla compare_two_cgMLST_profiles_plot.R -s SCOMP.tsv
 #
-# To run the script with specific output path, threshold and input paths for LCOMP and SCOMP
-#   Rscript --vanilla compare_two_cgMLST_profiles_plot.R -l LCOMP.tsv -s SCOMP.tsv - o out_path -t 95
+# To run the script with specific output path, threshold and input paths for GCOMP, LCOMP, and SCOMP
+#   Rscript --vanilla compare_two_cgMLST_profiles_plot.R -g GCOMP.tsv -l LCOMP.tsv -s SCOMP.tsv -o out_path -t 95
 #
 ################################################################################
 
@@ -39,6 +41,7 @@ library(optparse)
 
 # Making option list -----------------------------------------------------------
 option_list <- list(
+    make_option(c("-g", "--gcomp"), type = "character", help = "Path to GCOMP file [tsv].", metavar = "character"),
     make_option(c("-l", "--lcomp"), type = "character", help = "Path to LCOMP file [tsv].", metavar = "character"),
     make_option(c("-s", "--scomp"), type = "character", help = "Path to SCOMP file [tsv].", metavar = "character"),
     make_option(c("-t", "--threshold"), type = "numeric", help = "Threshould value [1-100]. Plot only loci with the same allele calls in less than provided threshould value. Default value = 90", metavar = "numeric", default = 90),
@@ -51,26 +54,50 @@ opt <- parse_args(opt_parser)
 
 
 # Check the provided option and execute the corresponding code -----------------
-if (is.null(opt$l) & is.null(opt$s)) {
+if (is.null(opt$g) & is.null(opt$l) & is.null(opt$s)) {
     print_help(opt_parser)
-    stop("Provide path to LCOMP or SCOMP file.")
+    stop("Provide path to GCOMP or LCOMP or SCOMP file.")
 }
 
-# Check if path to SCOMP is specified and if not use LCOMP to define it
+# Check if path to SCOMP is specified and if not use GCOMP or LCOMP to define it
 if (is.null(opt$s)) {
-    cat("SCOMP not specified.\nSCOMP will be searched in the same folder where is LCOMP.\n")
-    opt$s <- str_replace(opt$l, "_LCOMP.tsv$", "_SCOMP.tsv")
+    cat("SCOMP not specified.\n")
+    if (!is.null(opt$g)) {
+        cat("SCOMP will be searched in the same folder where is GCOMP.\n")
+        opt$s <- str_replace(opt$g, "_GCOMP.tsv$", "_SCOMP.tsv")
+    } else {
+        cat("SCOMP will be searched in the same folder where is LCOMP.\n")
+        opt$s <- str_replace(opt$l, "_LCOMP.tsv$", "_SCOMP.tsv")
+    }
 }
 
-# Check if path to LCOMP is specified and if not use SCOMP to define it
+# Check if path to LCOMP is specified and if not use GCOMP or SCOMP to define it
 if (is.null(opt$l)) {
-    cat("LCOMP not specified.\nSCOMP will be searched in the same folder where is SCOMP.\n")
-    opt$l <- str_replace(opt$s, "_SCOMP.tsv$", "_LCOMP.tsv")
+    cat("LCOMP not specified.\n")
+    if (!is.null(opt$g)) {
+        cat("LCOMP will be searched in the same folder where is GCOMP.\n")
+        opt$l <- str_replace(opt$g, "_GCOMP.tsv$", "_LCOMP.tsv")
+    } else {
+        cat("LCOMP will be searched in the same folder where is SCOMP.\n")
+        opt$l <- str_replace(opt$s, "_SCOMP.tsv$", "_LCOMP.tsv")
+    }
+}
+
+# Check if path to GCOMP is specified and if not use LCOMP or SCOMP to define it
+if (is.null(opt$g)) {
+    cat("GCOMP not specified.\n")
+    if (!is.null(opt$l)) {
+        cat("SCOMP will be searched in the same folder where is LCOMP.\n")
+        opt$g <- str_replace(opt$l, "_LCOMP.tsv$", "_GCOMP.tsv")
+    } else {
+        cat("SCOMP will be searched in the same folder where is LCOMP.\n")
+        opt$g <- str_replace(opt$s, "_SCOMP.tsv$", "_GCOMP.tsv")
+    }
 }
 
 # Check if output is specified and if not use input to define it
 if (is.null(opt$o)) {
-    opt$o <- dirname(opt$l)
+    opt$o <- dirname(opt$g)
     cat("Output not specified.\nOutput will be saved in: ", opt$o, "\n", sep = "")
 }
 
@@ -78,8 +105,10 @@ if (is.null(opt$o)) {
 
 # Load data --------------------------------------------------------------------
 cat("Reading in the data.\n")
+GCOMP <- read_tsv(opt$g, show_col_types = FALSE)
 LCOMP <- read_tsv(opt$l, show_col_types = FALSE)
 SCOMP <- read_tsv(opt$s, show_col_types = FALSE)
+
 
 # Define colors for the plot ---------------------------------------------------
 my_colors <-
@@ -92,6 +121,33 @@ my_colors <-
         "X1" = "#969696",
         "X2" = "#252525"
     )
+
+# Plot GENERAL -----------------------------------------------------------------
+cat("Plotting GCOMP\n")
+p_g <- GCOMP %>%
+    select(S, D, M, M1, M2, X1, X2) %>%
+    pivot_longer(cols = everything(), names_to = "Type", values_to = "Count") %>%
+    mutate(COMP = "P1_vs_P2") %>%
+    ggplot(aes(x = Count, y = COMP, fill = Type)) +
+    geom_bar(stat = "identity") +
+    labs(
+        title = "General Comparisons",
+        subtitle = paste0(
+            "\nProfile_1: ", GCOMP$Profile_1, "\n",
+            "Profile_2: ", GCOMP$Profile_2, "\n\n",
+            "NLP1: ", GCOMP$NLP1, "\n",
+            "NLP2: ", GCOMP$NLP2, "\n\n",
+            "NSP1: ", GCOMP$NSP1, "\n",
+            "NSP2: ", GCOMP$NSP2, "\n\n",
+            "LP1_notin_LP2: ", GCOMP$LP1_notin_LP2, "\n",
+            "LP2_notin_LP1: ", GCOMP$LP2_notin_LP1, "\n\n",
+            "Total_COMP: ", GCOMP$Total_COMP, "\n\n"
+        ),
+        x = "Total number of comparisons",
+        y = ""
+    ) +
+    scale_fill_manual(values = my_colors) +
+    theme_bw()
 
 # Plot SAMPLE ------------------------------------------------------------------
 cat("Plotting SCOMP.\n")
@@ -129,5 +185,6 @@ p_l <- LCOMP %>%
 
 # Save plots -------------------------------------------------------------------
 cat("Saving plots.\n")
+ggsave(paste0(opt$o, "/", tools::file_path_sans_ext(basename(opt$g)), ".png"), p_g, width = 20, height = 10)
 ggsave(paste0(opt$o, "/", tools::file_path_sans_ext(basename(opt$s)), ".png"), p_s, width = 10, height = 20)
 ggsave(paste0(opt$o, "/", tools::file_path_sans_ext(basename(opt$l)), ".png"), p_l, width = 10, height = 20)
